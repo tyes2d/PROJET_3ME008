53d6468b046abbf7c1400c92a3b85addefa99081
tyes2d <75027463+tyes2d@users.noreply.github.com>
2020-11-29 19:42:18 +0100

    Update main.c
    j'ai fais la Question 2 et optimisé pour le tracé de la question 3

--- main.c
+++ main.c
@@ -13,15 +13,11 @@
 #define nx 40.0
 
 
-
-int main() {
-    float h = 30/nx;      //h=L/nx=20/40
-    float dt = 30/(nx*nx);
-    float B[NX][NX];
-    float b[NX];
+//Question 1
+void Creation_B(float B[NX][NX], float b[NX], float h, float dt) {
     b[0]=dt/(h*h);
     for (int i=1; i<NX; i++) b[i]=0;
-    for (int i=0; i<NX; i++){
+    for (int i=0; i<NX; i++){           //on initialise B, on lui met une valeur par défaut de 0.0
         for (int j=0; j<NX; j++){
             B[i][j]=0.0;
         }
@@ -30,21 +26,57 @@ int main() {
         for (int j=0; j<NX; j++){
             if (i==j) B[i][j] = 1-2*dt/(h*h);
             else if (j==i+1 || j==i-1) B[i][j] = dt/(h*h);
-            else if (j==NX-2 && i==NX-1) B[i][j] = 2*dt/(h*h);
+            else if (j==NX-2 && i==NX-1) B[i][j] = 2*dt/(h*h); //on cré notre matrice B avec les valeurs quil faut
             else B[i][j] = 0;
         }
     }
-    printf("mat B : \n\n");
+    printf("mat B : \n\n");     //affichage de B
     for (int i=0; i<NX; i++){
         for (int j=0; j<NX; j++){
             printf(" %f ", B[i][j]);
         }
         printf("\n");
     }
-    printf("vect b : \n\n");
+    printf("vect b : \n\n");    //affichage de b
     for (int i=0; i<NX; i++){
         printf(" %f \n", b[i]);
     }
         
-    return 0;
+}
+
+void solution_numerique(float B[NX][NX], float b[NX], float h, float dt, float Tinitiale, float Tfinale, int Tstop, float T[NX]){
+    float res=0.0;
+    for (int i=1; i<NX-1; i++) T[i]=Tinitiale;
+    T[0]=Tfinale;
+    T[NX-1]=Tfinale;
+    for (int n=0; n*dt<Tstop; n++){ // n=Tstop/dt : nombre d'échantillon temporels sur lesquels il faut trouver T jusqu'au temps Tstop
+        for (int i=1; i<NX-1;i++){  //On applique ici la formule T^n+1 = [B]*T^n + [b]
+            res=0.0;
+            for (int j=0; j<NX;j++) res += B[i][j]*T[j];
+            T[i] = res+b[i];
+        }
+    }
+}
+
+int main(){
+    float h = 30/nx;      //h=L/nx=20/40
+    float dt = 30/(nx*nx);
+    float B[NX][NX];
+    float b[NX];
+    float Ti=750;
+    float Tf=25;
+    int Ts;
+    float T[NX];
+    for (int i=0; i<NX; i++) T[i]=0;
+    
+    printf("Au bout de combien de temps la plaque doit-elle etre retirée de l'eau (en seconde)? \n");
+    scanf("%d", &Ts);
+    Creation_B(B, b, h, dt);
+    solution_numerique(B, b, h, dt, Ti, Tf, Ts, T);
+    printf("vect T : \n\n");    //affichage de T à Ts
+    for (int i=0; i<NX; i++){
+        printf(" %f \n", T[i]);
+    }
+    
+    
 }
